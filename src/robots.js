4e0b57fcb72f33378ba8eabed8b74a43a2e1eaa7
Erich Malberg <emalberg@live.com>
2023-02-18 20:41:20 -0700

    robofriens setup for ZTM courses

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Hailie Fine',
+        username: 'Hailstorm',
+        email: 'hailie.fine19@alumni.colostate.edu'
+    },
+    {
+        id: 2,
+        name: 'Jezzabell June',
+        username: 'PsycoKat',
+        email: 'PsycoKitty@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Mesma Lambkin',
+        username: 'Destructo',
+        email: 'DestroySecurity@aus.com'
+    },
+    {
+        id: 4,
+        name: 'Margrett Fennel',
+        username: 'F3NN3L',
+        email: 'M.Fennel1@live.com'
+    }
+]
