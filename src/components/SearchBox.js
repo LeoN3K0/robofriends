05d7a3b0ca190eea2008629d128d83ebb18df5f3
Erich Malberg <emalberg@live.com>
2023-02-20 18:40:31 -0700

    Finalizing with some organizing and cleaning up of the code

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,9 +1,9 @@
 import React from "react";
+import './SearchBox.css'
 
 const SearchBox = ({ searchfield, searchChange }) => {
     return (
         <div className="pa2">
-
             <input
             className="pa3 ba b--green bg-lightest-blue"
             type='search' 
